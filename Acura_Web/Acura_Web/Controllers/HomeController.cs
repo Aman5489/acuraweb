63908b68e0275eb756c57111fb2c804e4a53bb1b
Aman <aman5489@gmail.com>
2020-06-16 13:00:27 +0530

    master: Added Acura web portal

---
+++ Acura_Web/Acura_Web/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Acura_Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        public ActionResult OurClient()
+        {
+            return View();
+        }
+        public ActionResult Services()
+        {
+            return View();
+        }
+    }
+}
---
+++ Acura_Web/Acura_Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Acura_Web.MvcApplication" Language="C#" %>
